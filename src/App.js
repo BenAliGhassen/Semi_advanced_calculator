78ef6e1e9b7fcea2dbf2feb1ac604fd9a3f7fb85
BenAliGhassen <125483783+BenAliGhassen@users.noreply.github.com>
2023-09-29 20:54:47 +0100

    App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [value,setValue]=useState("");
+ 
+  function reset(){
+    setValue("");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className='screen'>
+    <input type='text' className='display' value={value}/>
+    </div>
+    <div className="container">  
+      <div className='calculator_nums'>
+        <input type='button' className='btn_nums' value={1} onClick={e => setValue(value+e.target.value)}/>
+        <input type='button' className='btn_nums' value={2} onClick={e => setValue(value+e.target.value)}/>
+        <input type='button' className='btn_nums' value={3} onClick={e => setValue(value+e.target.value)}/>
+        <input type='button' className='btn_nums' value={4} onClick={e => setValue(value+e.target.value)}/>
+        <input type='button' className='btn_nums' value={5} onClick={e => setValue(value+e.target.value)}/>
+        <input type='button' className='btn_nums' value={6} onClick={e => setValue(value+e.target.value)}/>
+        <input type='button' className='btn_nums' value={7} onClick={e => setValue(value+e.target.value)}/>
+        <input type='button' className='btn_nums' value={8} onClick={e => setValue(value+e.target.value)}/>
+        <input type='button' className='btn_nums' value={9} onClick={e => setValue(value+e.target.value)}/>
+        <input type='button' className='btn_nums' value={0} onClick={e => setValue(value+e.target.value)}/>
+      </div>
+
+      <div className='operation_container'>
+      <input type='button' className='btn_nums' value={"+"} onClick={e => setValue(value+e.target.value)}/>
+        <input type='button' className='btn_nums' value={"-"} onClick={e => setValue(value+e.target.value)}/>
+        <input type='button' className='btn_nums' value={"*"} onClick={e => setValue(value+e.target.value)}/>
+        <input type='button' className='btn_nums' value={"/"} onClick={e => setValue(value+e.target.value)}/>
+        <input type='button' className='btn_nums' value={"="} onClick={e => setValue(value+"="+eval(value))}/>
+        <input type='button' className='btn_nums' value={"C"} onClick={reset}/>
+
+      </div>
+    </div>
     </div>
   );
 }
