20df6aacaef49143575d00bf929dc6ec5bd3af32
BenAliGhassen <125483783+BenAliGhassen@users.noreply.github.com>
2023-10-05 00:27:40 +0100

    App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,61 @@ function App() {
   function reset(){
     setValue("");
   };
+  function calculate() {
+    const operators = [];
+    const numbers = [];
+  
+    const precedence = {
+      '+': 1,
+      '-': 1,
+      '*': 2,
+      '/': 2,
+    };
+  
+    const tokens = value.match(/(\d+|[+\-*/])/g);
+  
+    tokens.forEach(token => {
+      if (!isNaN(token)) {
+        numbers.push(parseFloat(token));
+      } else if (token in precedence) {
+        while (
+          operators.length > 0 &&
+          precedence[operators[operators.length - 1]] >= precedence[token]
+        ) {
+          const operator = operators.pop();
+          const num2 = numbers.pop();
+          const num1 = numbers.pop();
+          if (operator === '+') {
+            numbers.push(num1 + num2);
+          } else if (operator === '-') {
+            numbers.push(num1 - num2);
+          } else if (operator === '*') {
+            numbers.push(num1 * num2);
+          } else if (operator === '/') {
+            numbers.push(num1 / num2);
+          }
+        }
+        operators.push(token);
+      }
+    });
+  
+    while (operators.length > 0) {
+      const operator = operators.pop();
+      const num2 = numbers.pop();
+      const num1 = numbers.pop();
+      if (operator === '+') {
+        numbers.push(num1 + num2);
+      } else if (operator === '-') {
+        numbers.push(num1 - num2);
+      } else if (operator === '*') {
+        numbers.push(num1 * num2);
+      } else if (operator === '/') {
+        numbers.push(num1 / num2);
+      }
+    }
+    setValue(numbers[0]);
+  }
+  
   return (
     <div>
     <div className='screen'>
@@ -31,7 +86,7 @@ function App() {
         <input type='button' className='btn_nums' value={"-"} onClick={e => setValue(value+e.target.value)}/>
         <input type='button' className='btn_nums' value={"*"} onClick={e => setValue(value+e.target.value)}/>
         <input type='button' className='btn_nums' value={"/"} onClick={e => setValue(value+e.target.value)}/>
-        <input type='button' className='btn_nums' value={"="} onClick={e => setValue(value+"="+eval(value))}/>
+        <input type='button' className='btn_nums' value={"="} onClick={calculate}/>
         <input type='button' className='btn_nums' value={"C"} onClick={reset}/>
 
       </div>
